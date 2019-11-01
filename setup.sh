707bf015b4c4405658bd2359b58c17a6a0c74e51
taehokang <ksm07091@gmail.com>
2019-11-01 19:33:46 +0900

    spacevim 적용

--- setup.sh
+++ setup.sh
@@ -3,9 +3,14 @@
 sudo apt-get install tmux -qq
 sudo apt-get install vim -qq
 cp tmux.conf ../.tmux.conf
-cp vimrc ../.vimrc
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# spacevim style
+curl -sLf https://spacevim.org/install.sh | bash
+cp init.toml ../.SpaceVim.d/init.toml
+vim
 
-vim -c PluginInstall
+# old vim style
+#cp vimrc ../.vimrc
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim -c PluginInstall
 
