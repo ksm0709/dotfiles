8b8f70c4e6719808d2760b2b7095ac68d8c482fd
taehokang <ksm07091@gmail.com>
2019-11-22 13:52:47 +0900

    latest

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,11 +8,8 @@ sudo apt-get install neovim
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --config editor
 
 # spacevim
 curl -sLf https://spacevim.org/install.sh | bash
