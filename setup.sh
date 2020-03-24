3f8b620e4e7406865816cf38e8b23c2c1845e335
TaehoKang <ksm07091@gmail.com>
2020-03-24 14:19:09 +0900

    latest

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ sudo apt-get install vim -qq
 cp tmux.conf ../.tmux.conf
 cp vimrc ../.vimrc
 
+mkdir ${HOME}/.vim
+mkdir ${HOME}/.vim/bundle
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim -c PluginInstall
