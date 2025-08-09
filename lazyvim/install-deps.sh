b8c2d148917c15112398c4e3bf29053622f992c7
Taeho TabS8 <ksm07091@gmail.com>
2025-08-10 00:56:32 +0900

    latest

--- lazyvim/install-deps.sh
+++ lazyvim/install-deps.sh
@@ -15,7 +15,14 @@ pkg_list=( \
 
 #if its termux, use pkg
 if [ -f /data/data/com.termux/files/usr/bin/pkg ]; then
-  pkg install "${pkg_list[@]}"
+  # ignore unexisting packages
+  for pkg in "${pkg_list[@]}"; do
+    if ! pkg info "$pkg" > /dev/null 2>&1; then
+      echo "Package $pkg not found. Skipping..."
+      continue
+    fi
+    pkg install "$pkg"
+  done
   exit 0
 fi
 sudo apt-get install "${pkg_list[@]}"
