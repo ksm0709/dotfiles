37a006b89cd925f6a0227f4bf272c972f5f766fc
Taeho TabS8 <ksm07091@gmail.com>
2025-08-10 00:52:49 +0900

    update

--- lazyvim/install-deps.sh
+++ lazyvim/install-deps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Apt deps
-sudo apt-get install -y \
-	clang \
-	make \
-	wget \
-	luarocks \
-	fzf \
-	imagemagick \
-	lazygit \
-	fd-find \
-	chromium-browser
+pkg_list = (
+  clang
+  make
+  wget
+  luarocks
+  fzf
+  imagemagick
+  lazygit
+  fd-find
+  chromium
+)
+
+#if its termux, use pkg
+if [ -f /data/data/com.termux/files/usr/bin/pkg ]; then
+  pkg install "${pkg_list[@]}"
+  exit 0
+
+sudo apt-get install "${pkg_list[@]}"
