4f9ddadaf71982e3fc60bc3283a19dcef53d2223
Taeho TabS8 <ksm07091@gmail.com>
2025-08-10 01:12:10 +0900

    update

--- lazyvim/install-deps.sh
+++ lazyvim/install-deps.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
 # Apt deps
-pkg_list=( \
-  clang \
-  make \
-  wget \
-  luarocks \
-  fzf \
-  imagemagick \
-  lazygit \
-  fd-find \
-  chromium \
+pkg_list=(
+	clang
+	make
+	wget
+	luarocks
+	fzf
+	imagemagick
+	lazygit
+	fd-find
+	chromium
 )
 
 #if its termux, use pkg
 if [ -f /data/data/com.termux/files/usr/bin/pkg ]; then
-  # ignore unexisting packages
-  for pkg in "${pkg_list[@]}"; do
-    if ! pkg info "$pkg" > /dev/null 2>&1; then
-      echo "Package $pkg not found. Skipping..."
-      continue
-    fi
-    pkg install "$pkg"
-  done
-  exit 0
+	# Additional list for termux
+	pkg_list+=(lua-language-server)
+
+	# ignore unexisting packages
+	for pkg in "${pkg_list[@]}"; do
+		if ! pkg info "$pkg" >/dev/null 2>&1; then
+			echo "Package $pkg not found. Skipping..."
+			continue
+		fi
+		pkg install -y "$pkg"
+	done
+
+	exit 0
 fi
 sudo apt-get install "${pkg_list[@]}"
